f0820a3ebc0c82d4b3bcadd55b5c4658347a7850
Randall <randall.erasmus@interfront.co.za>
2019-12-10 19:00:01 +0200

    initial commit

---
+++ main/java/za/co/byteservices/icontractbackend/IcontractbackendApplication.java
@@ -0,0 +1,24 @@
+package za.co.byteservices.icontractbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class IcontractbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcontractbackendApplication.class, args);
+    }
+
+    @RestController
+    public class HelloController {
+
+        @GetMapping("/")
+        String hello() {
+            return "Hello World";
+        }
+
+    }
+}
---
+++ test/java/za/co/byteservices/icontractbackend/IcontractbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.byteservices.icontractbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcontractbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
